ac7e712036edb77abb6a3d09da4e11d1f3dc6e75
Rohan Datar <me@rohandatar.com>
2024-12-28 19:33:58 -0600

    set static addr

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,21 @@
     pkgs.neovim
   ];
 
+  networking = {
+    interfaces = {
+      enp1s0.ipv4.addresses = [
+        {
+          address = "10.10.1.11";
+          prefixLength = 19;
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "10.10.0.1";
+      interface = "enp1s0";
+    };
+  };
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYDkyHobLUDOAkNqHxcOkVScdCclKG6m6Az7OT/NAd3"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2wVTZEDwBCIvmTEiKj3pUmhOR+W9qknzbVTXhM25h6"
